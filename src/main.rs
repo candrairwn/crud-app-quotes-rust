dc6008b766bb4b6e3c2a4915db4bcd5bce12a587
candrairwn <candrai1276@gmail.com>
2023-11-19 19:14:55 +0700

    add: post quotes

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-use axum::http;
-use axum::routing::{get, Router};
+mod handlers;
+use axum::routing::{get, post, Router};
+use sqlx::postgres::PgPoolOptions;
+use std::{env, error};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+async fn main() -> Result<(), Box<dyn error::Error>>{
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let addr = format!("0.0.0.0:{}", port);
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&database_url)
+    .await?;
+
     println!("Listening on {}", addr);
 
-    let app = Router::new().route("/", get(healt));
+    let app = Router::new()
+        .route("/", get(handlers::health))
+        .route("/quotes", post(handlers::create_quote))
+        .with_state(pool);
 
-    axum::Server::bind(&addr.parse().unwrap())
-    .serve(app.into_make_service())
-    .await
-    .unwrap();
+    axum::Server::bind(&addr.parse()?)
+        .serve(app.into_make_service())
+        .await?;
+    
 
     Ok(())
-}
-
-async fn healt() -> http::Response<String> {
-    http::Response::new("Hello, World!".into())
 }
